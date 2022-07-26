04e4d4aef8e55a00039fb365945adb7bbcfea296
corymfonteyne <corymfonteyne@gmail.com>
2022-07-25 21:14:03 -0400

    test1

--- Task Manager Project/scripts/app.js
+++ Task Manager Project/scripts/app.js
@@ -3,25 +3,49 @@ let nonImportantIconClass = "fa-regular fa-star";
 let isImportant = false;
 
 
-function toggleImportant(){
+function toggleImportant() {
     console.log('Icon Clicked');
-}
     
-if (!isImportant) {
-    $("#icon-important").removeClass(importantIconClass);//remove non-important icon class
-    $("#icon-important").addClass(nonImportantIconClass);//add important icon class
-    isImportant = true;
-}
-else{
-    $("#icon-important").removeClass(nonImportantIconClass);
-    $("#icon-important").addClass(importantIconClass);
-    isImportant = false;
+    if (!isImportant) {
+        $("#icon-important").removeClass(importantIconClass);//remove non-important icon class
+        $("#icon-important").addClass(nonImportantIconClass);//add important icon class
+        isImportant = true;
+    }
+    else{
+        $("#icon-important").removeClass(nonImportantIconClass);
+        $("#icon-important").addClass(importantIconClass);
+        isImportant = false;
+    }
 }
 
-function init(){
+function saveTask(){
+    console.log("Task Saved!");
+
+    let title = $("#txtTitle").val();
+    let dueDate = $("#txtDueDate").val();
+    let description = $("#txtDescription").val();
+    let color = $("#selColor").val();
+    let emoji = $("#selEmoji").val();
+    let location = $("#txtLocation").val();
+    let status = $("#selStatus").val();
+    let notifications = $("#chkNotification").prop("checked");
+
+    console.log(title,dueDate,description,color,emoji,location,status,notifications)
+
+}
+
+function hideTask(){
+    console.log("Taskbar Hidden");
+}
+
+
+
+function init() {
     console.log("Task Manager Page!");
     $("#icon-important").click(toggleImportant);
+    $("#save-button").click(saveTask);
+    $("#hide-task").click(hideTask);
 }
-$("#icon-important").click(toggleImportant);
+
 
 window.onload = init;
